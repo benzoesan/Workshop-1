3e8fbabe6a1e57ca3cacc4fb29f514528ff5065d
benzoesan <117587746+benzoesan@users.noreply.github.com>
2023-01-14 18:53:53 +0100

    test2

---
+++ TaskManager1/src/main/java/pl/coderslab/Main.java
@@ -0,0 +1,12 @@
+package pl.coderslab;
+
+public class Main {
+    static final String FILE_NAME = "tasks.csv";
+    static final String[] OPTIONS = {"add", "remove", "list", "exit"};
+
+    public static void main(String[] args) {
+        System.out.println("cp");
+        
+
+    }
+}
